d5646cd2b62c73351b19a7e7e791904f7c8900cd
perejro <perejro@centro.team>
2022-10-20 10:56:49 +0300

    pdo

--- controllers/Articles.php
+++ controllers/Articles.php
@@ -1,35 +1,39 @@
 <?php
-
-namespace Controllers;
-use Models\Article;
-use View\View;
-
-class Articles implements BaseController
-{
-  private string $name = 'Article page';
-  private $model;
-
-  public function __construct()
+  
+  namespace Controllers;
+  
+  use Models\Article;
+  use View\View;
+  
+  class Articles implements BaseController
   {
-    $this->model = new Article;
+    private string $name = 'Article page';
+    private $model;
+    
+    public function __construct()
+    {
+      $this->model = new Article;
+    }
+    
+    public function index(): void
+    {
+      $data['title'] = $this->name;
+      $data['articles'] = $this->model->getAll();
+      View::generate('articles', $data);
+    }
+    
+    public function create(array $data): void
+    {
+      $this->model->create($data);
+    }
+    
+    public function edit(string $id, string $field, string $value): void
+    {
+      $this->model->edit($id, $field, $value);
+    }
+    
+    public function delete(string $id): void
+    {
+      $this->model->delete($id);
+    }
   }
-
-  public function index(): void
-  {
-    $data['title'] = $this->name;
-    $data['articles'] = $this->model->getAll();
-    View::generate('articles', $data);
-  }
-
-  public function create(array $data): void {
-    $this->model->create($data);
-  }
-
-  public function edit(string $id, string $field, string $value): void {
-    $this->model->update($id, $field, $value);
-  }
-
-  public function delete(string $id): void {
-    $this->model->delete($id);
-  }
-}
--- controllers/Home.php
+++ controllers/Home.php
@@ -1,16 +1,15 @@
 <?php
+  
   namespace Controllers;
-  use Models\Article;
+  
   use View\View;
-
+  
   class Home implements BaseController
   {
     private string $name = 'Home route';
-
+    
     public function index(): void
     {
-      $article = new Article;
-      $data['articles'] = $article->getAll();
-      View::generate('home', $data);
+      View::generate('home', []);
     }
   }
