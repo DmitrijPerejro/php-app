679e8425b93e1242fb9443e8791820f6963ae0f9
perejro <perejro@centro.team>
2022-10-20 11:30:07 +0300

    change response code

--- controllers/Registration.php
+++ controllers/Registration.php
@@ -23,7 +23,7 @@
       
       $this->registration($login, $email, $password);
       header('Content-Type: application/json; charset=utf-8');
-      http_response_code(400);
+      http_response_code(200);
       echo 'SUCCESS';
     }
     
