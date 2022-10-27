540510708257c277b0bf674a99f6adccb48ca5bc
perejro <perejro@centro.team>
2022-10-27 19:08:51 +0300

    host

--- core/orm/config.php
+++ core/orm/config.php
@@ -1,7 +1,7 @@
 <?php
   return [
     'db' => 'u226448678_app',
-    'host' => 'app.perejro.me',
+    'host' => 'perejro.me',
     'username' => 'u226448678_root',
     'password' => '3Jf]1^ZLm^Hm',
   ];
